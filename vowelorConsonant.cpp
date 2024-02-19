4d3a4fd765d2f716b2ea87782fd7174d808afe25
sunil_github <117855897+sunilroutgithub@users.noreply.github.com>
2024-02-19 18:24:36 +0530

    Add files via upload

---
+++ vowelorConsonant.cpp
@@ -0,0 +1,18 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+    char ch;
+    cin >>ch;
+    if(isalpha(ch)){
+    if(ch =='a' || ch =='e' || ch =='i' || ch =='o' || ch =='u' || ch =='A' || ch =='E' || ch =='I' || ch =='O' || ch =='U'){
+        cout << "Vowel"<<endl;
+    }
+    else{
+        cout << "Consonant"<<endl;
+     }
+   } 
+   else{
+    cout<<ch<<" is not an aplhabet."<<endl;
+   }
+   return 0;
+ }
